5591c51d1150b098e4d07be588b92a66a35cb130
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-08 13:01:53 +0100

    Answered the theory questions on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding occurs when a function containing the `this` keyword was invoked on the global
+*    object without any binding. With this, the `this` keyword refers to the global object which is 
+*    window object in this case of the DOM.
+* 2. Implicit binding is when the function containing the `this` keyword was invoked on the containing
+*    object using the dot notation. The `this` keyword refers back to the object.
+* 3. New Binding occurs when the `new` keyword is used to instatiate a new instance of the object containing the 
+*    `this` keyword. In this case, the `this` keyword refers to the newly created object.
+* 4. Explicit Binding occurs when a function is specifically binded to an object using .apply(), .call() or .bind()
 *
 * write out a code example of each explanation above
 */
